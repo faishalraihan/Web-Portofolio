5c8b5903021fa3486316aa8835e8dc7f4600f437
redxx <faishalraihan123@gmail.com>
2020-10-23 00:07:10 +0700

    Add Speciality section

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 function changeImage() {
-        var BackgroundImg = ["./img/daniel-leone-v7daTKlZzaw-unsplash.jpg", "./img/photo-1526344966-89049886b28d.jpeg", "./img/moraine_lake_landscape_at_banff_national_park_5k-1920x1080.jpg"];
-        var i = Math.floor((Math.random() * 3));
+        var BackgroundImg = ["./img/adam-mescher-47Hw5d4AxKM-unsplash.jpg", "./img/eberhard-grossgasteiger-cs0sK0gzqCU-unsplash.jpg", "./img/elvis-bekmanis-g9qwoPiS0nY-unsplash.jpg", "./img/jonatan-pie-h8nxGssjQXs-unsplash.jpg", "./img/elvis-bekmanis-g9qwoPiS0nY-unsplash.jpg", "./img/joshua-earle-C6duwascOEA-unsplash.jpg"];
+        var i = Math.floor((Math.random() * 5));
         document.getElementById("header").style.backgroundImage = 'url("' + BackgroundImg[i] + '")';
 }
 window.setInterval(changeImage, 5000);
